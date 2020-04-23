7b52c3262e5609e66e3ac05dc6af43090e9fe5bc
Matthew Kinzler <matthew.kinzler@gmail.com>
2020-04-23 13:00:04 -0400

    refactor argument parser in db browser

--- db_sd_reader.py
+++ db_sd_reader.py
@@ -1,22 +1,32 @@
 import sqlite3
 import argparse
+import sys
 
-def arg_parser():
+def arg_parser(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('db_name', type=str, help='Database file name')
     parser.add_argument('table_name', type=str, help='Table name in database')
     parser.add_argument('-t', '--time', default=-1, type=int, help='Show orders for transfer time greater than this many seconds')
-    args = parser.parse_args()
-    return args
+    parser.add_argument('-o', '--ordrnum', default=-1, type=str, help='Option to select a specific order')
+    return parser.parse_args()
+    
 
-def read_from_db():
-    args = arg_parser()
+def read_from_db(args):
     conn = sqlite3.connect(args.db_name)
     c = conn.cursor()
-    c.execute("select * from {} where time >= {} order by time desc".format(args.table_name, args.time))
+
+    # sql select of ordrnum is given
+    if args.ordrnum:
+        c.execute("select * from {} where ordrnum = '{}'".format(args.table_name, args.ordrnum))
+    # otherwise, select all
+    else:
+        c.execute("select * from {} where time >= {} order by time desc".format(args.table_name, args.time))
+    
     data = c.fetchall()
     for date, mops, time in data:
         print date, mops, time
     return
 
-read_from_db()
+def main():
+    parser = arg_parser(sys.argv[1:])
+    read_from_db(parser)
