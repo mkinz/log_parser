0dcc099e0a220f51671d58d70dec1da1f5d96017
Matthew Kinzler <matthew.kinzler@gmail.com>
2020-05-14 20:33:22 -0400

    fix dependency injection

--- db_sd_reader.py
+++ db_sd_reader.py
@@ -2,34 +2,58 @@ import sqlite3
 import argparse
 import sys
 
-def arg_parser(args):
-    parser = argparse.ArgumentParser()
+
+def arg_parser(args, parser):
     parser.add_argument('db_name', type=str, help='Database file name')
     parser.add_argument('table_name', type=str, help='Table name in database')
-    parser.add_argument('-t', '--time', default=-1, type=int, help='Show orders for transfer time greater than this many seconds')
-    parser.add_argument('-o', '--ordrnum', default=-1, type=str, help='Option to select a specific order')
-    return parser.parse_args()
-    
+    parser.add_argument('-t', '--time', default=-1, type=int,
+                        help='Show orders for transfer time greater than this many seconds')
+    parser.add_argument('-o', '--ordrnum', type=str, help='Optional ordrnum')
+    parser.add_argument('-d', '--date', type=str, help='Optional date')
 
-def read_from_db(args):
+    return parser.parse_args(args)
+
+
+def connect_to_db(args):
     conn = sqlite3.connect(args.db_name)
-    c = conn.cursor()
+    return conn
 
-    # sql select of ordrnum is given
+
+def get_cursor(conn):
+    c = conn.cursor()
+    return c
+
+
+def read_from_db(args, cursor):
+    # sql select if ordrnum is given
     if args.ordrnum:
-        c.execute("select * from {} where ordrnum = '{}'".format(args.table_name, args.ordrnum))
+        cursor.execute("select * from {} where ordrnum = '{}'".format(args.table_name, args.ordrnum))
+    elif (args.date and args.time):
+        cursor.execute(
+            "select * from {} where date like ('%{}%') and time >= {} order by time desc".format(args.table_name,
+                                                                                                 args.date, args.time))
+    elif args.date:
+        cursor.execute(
+            "select * from {} where date like ('%{}%') order by time desc".format(args.table_name, args.date))
     # otherwise, select all
     else:
-        c.execute("select * from {} where time >= {} order by time desc".format(args.table_name, args.time))
-    
-    data = c.fetchall()
-    for date, mops, time in data:
-        print date, mops, time
-    return
+        cursor.execute("select * from {} where time >= {} order by time desc".format(args.table_name, args.time))
+
+    # print out the data
+    data = cursor.fetchall()
+    return data
+
 
 def main():
-    parser = arg_parser(sys.argv[1:])
-    read_from_db(parser)
+    # get command line arguments and feed them into the db reader func
+    parser = arg_parser(args=sys.argv[1:], parser=argparse.ArgumentParser())
+
+    connect_to_db(parser)
+    get_cursor(connect_to_db(parser))
+
+    for date, mops, time in read_from_db(parser, get_cursor(connect_to_db(parser))):
+        print(date, mops, time)
+
 
 if __name__ == '__main__':
     main()
